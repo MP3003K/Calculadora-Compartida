a30b39ff539cf74ad56a0845048e1fff25cd6c1b
usuario169 <mijailpalomino@upeu.edu.pe>
2020-04-27 17:36:40 -0500

    paso2

---
+++ java/src/app/App.java
@@ -0,0 +1,10 @@
+package app;
+
+import java.util.Scanner;
+
+public class App {
+    public static Scanner teclado = new Scanner(System.in);
+    public static void main(String[] args) throws Exception {
+        
+    }
+}
